e87dc750511a40d15bf11c0e254ad377a8a9d4f1
DFKorn <d1man2192@mail.ru>
2025-08-27 20:21:55 +0300

    add next-themes library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@/services/clerk/components/ClerkProvider";
+import { ThemeProvider } from "next-themes";
 
 const outfitSans = Outfit({
   variable: "--font-outfit-sans",
@@ -22,7 +23,14 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${outfitSans.variable} antialiased font-sans`}>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableColorScheme
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
