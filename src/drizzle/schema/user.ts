2bc22eb9b268f23503ac0b6726d157c395792ef8
DFKorn <d1man2192@mail.ru>
2025-08-28 11:46:19 +0300

    create tables

--- src/drizzle/schema/user.ts
+++ src/drizzle/schema/user.ts
@@ -1,5 +1,7 @@
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 import { createdAt, updatedAt } from "../schemaHelpers";
+import { relations } from "drizzle-orm/relations";
+import { JobInfoTable } from "./jobInfo";
 
 export const UserTable = pgTable("users", {
   id: varchar().primaryKey(),
@@ -9,3 +11,7 @@ export const UserTable = pgTable("users", {
   createdAt,
   updatedAt,
 });
+
+export const userRelations = relations(UserTable, ({ many }) => ({
+  jobInfos: many(JobInfoTable),
+}));
