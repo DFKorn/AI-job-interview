3881f202c48a3e4cd889c4635749822f5a74d3e8
DFKorn <d1man2192@mail.ru>
2025-09-03 11:16:32 +0300

    setup Clerk billing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { PricingTable } from "@/services/clerk/components/PricingTable";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <SignInButton />
-      <UserButton />
-      <ThemeToggle />
-    </>
+    <div className="space-y-4 p-4">
+      <div className="flex items-center gap-4">
+        <SignInButton />
+        <UserButton />
+        <ThemeToggle />
+      </div>
+      <PricingTable />
+    </div>
   );
 }
